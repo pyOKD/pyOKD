140ea9e22d1e38cbdb8a008f53c7382e6b17dbc2
pyOKD <pyokd123@gmail.com>
2016-08-28 07:05:25 -0700

    Replace pickle with JSON. Cleaned up some nonsense

--- pyOKD.py
+++ pyOKD.py
@@ -2,27 +2,19 @@ import asyncore
 import socket
 import time
 import subprocess
-import cPickle as pickle
+import json
 
 class DATABASE():
     def __init__(self):
-        with open("saved_buffer.pkl", "rb") as f:
-            self.info1 = pickle.load(f)
-
-    
+        with open("saved_buffer.json", "r") as f:
+            self.info1 = json.load(f)
 
 class EchoHandler(asyncore.dispatcher_with_send):
     
-    def __init__(self, sock, data_class, addr):
+    def __init__(self, sock, database, addr):
         asyncore.dispatcher_with_send.__init__(self, sock)
-        self.DATABASE = data_class
+        self.DATABASE = database
         self.conn_info = addr
-        self.dispatch = {
-                "set" : self.set_buffer,
-                "get" : self.get_buffer,
-                "save" : self.save_buffer,
-                "load" : self.load_buffer,
-            }
 
     def handle_read(self):
         data = self.recv(8192)
@@ -32,13 +24,16 @@ class EchoHandler(asyncore.dispatcher_with_send):
                 self.send(self.save_buffer())
             elif command[0] == 'load':
                 self.send(self.load_buffer())
-            elif command[0] in self.dispatch.keys() and len(command) > 1:
+            elif command[0] == 'get':
                 if len(command) == 2:
-                    self.send(self.dispatch[command[0]](command[1]))
-                elif len(command) == 3:
-                    self.send(self.dispatch[command[0]](command[1], command[2]))
+                    self.send(self.get_buffer(command[1]))
                 else:
-                    self.send("Too many arguments")
+                    self.send("get requires 2 arguments")
+            elif command[0] == 'set':
+                if len(command) == 3:
+                    self.send(self.set_buffer(command[1], command[2]))
+                else:
+                    self.send("set requires 3 arguments")
             else:
                 self.send("Does not exist")
 
@@ -60,13 +55,13 @@ class EchoHandler(asyncore.dispatcher_with_send):
         else:
             return "Error: This key does not exist"
     def save_buffer(self, arg1= None, arg2 = None):
-        with open("saved_buffer.pkl", "wb") as f:
-            pickle.dump(self.DATABASE.info1, f)
+        with open("saved_buffer.json", "w") as f:
+            json.dump(self.DATABASE.info1, f)
             return "Saved buffer"
 
     def load_buffer(self, arg1 = None, arg2 = None):
-        with open("saved_buffer.pkl", "rb") as f:
-            self.DATABASE.info1 = pickle.load(f)
+        with open("saved_buffer.json", "r") as f:
+            self.DATABASE.info1 = json.load(f)
             return "Loaded Buffer"
 
 
@@ -92,7 +87,7 @@ class Server(asyncore.dispatcher):
         asyncore.loop()
         print "loop ended"
 
-class data_client():
+class client():
     def __init__(self):
         self.host = '10.0.0.24'
         self.port = 8080 
@@ -100,28 +95,28 @@ class data_client():
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         self.s.connect((self.host,self.port))
 
-    def data_command(self, command):
+    def command(self, command):
         self.s.send(command) 
         return self.s.recv(self.size)
 
-    def data_get(self, index):
-        return self.data_command("get " + str(index))
+    def get(self, index):
+        return self.command("get " + str(index))
 
-    def data_set(self, index, data):
-        return self.data_command("set " + str(index) + " " + str(data))
+    def set(self, index, data):
+        return self.command("set " + str(index) + " " + str(data))
 
-    def data_move(self, index, delta):
-        data = int(self.data_get(index))
-        self.data_set(index, data + delta)
+    def move(self, index, delta):
+        data = int(self.get(index))
+        self.set(index, data + delta)
         return "Data move"
 
-    def data_load(self, index):
-        data = self.data_get(index)
-        return pickle.loads(data)
+    def load(self, index):
+        data = self.get(index)
+        return json.loads(data)
 
-    def data_store(self, index, data):
-        storage = pickle.dumps(data)
-        self.data_set(index, storage)
+    def store(self, index, data):
+        storage = json.dumps(data)
+        self.set(index, storage)
 
 if __name__ == "__main__":
     Server('0.0.0.0', 8080).loop()
