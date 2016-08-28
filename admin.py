4e3ed245bdb480e3eb26d4ccf694a0ed064a6b95
pyOKD <pyokd123@gmail.com>
2016-08-28 07:06:17 -0700

    Fixed method names

--- admin.py
+++ admin.py
@@ -1,14 +1,14 @@
 import pyOKD
 
 if __name__ == "__main__":
-    user = pyOKD.data_client()
+    user = pyOKD.client()
     while(True):
         user_command = raw_input("Client: ")
         if  user_command == "quit":
             user.s.close()
             break
         else:
-            print user.data_command(user_command)
+            print user.command(user_command)
 
         
         
